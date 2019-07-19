cd1c1f75619d664a4ec763bb230aa1c61c7e7ecb
Purvi Kanal <purvikanal92@gmail.com>
2019-07-19 13:38:45 -0400

    added backend connection and updated README

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? ""
+    : `http://${process.env.REACT_APP_PROJECT_NAME}-backend.bridgeschoolapp.io`;
+
+const request = (endpoint, method = "GET") =>
+  fetch(`${API_BASE_URL}/${endpoint}`);
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      return await request("health");
+    };
+    fetchData();
+  });
   return (
     <div className="App">
       <header className="App-header">
