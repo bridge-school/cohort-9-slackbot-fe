b45d6dd830ad4ecb33f3618d15a2faa284b2475d
Zena Jun <zena.jun@gmail.com>
2019-11-26 21:12:49 -0500

    removed slash in url

--- src/backend-request/index.js
+++ src/backend-request/index.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8001"
-    : `https://slackbot-backend.herokuapp.com/`;
+    : `https://slackbot-backend.herokuapp.com`;
 
 export const request = (endpoint, method = "GET", body) =>
   fetch(`${API_BASE_URL}/${endpoint}`, {
