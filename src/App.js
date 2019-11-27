bfb122099d613011e413c2e0d97228a1df4e2566
Zena Jun <zena.jun@gmail.com>
2019-11-26 20:33:53 -0500

    nested body font styled under html

--- src/App.js
+++ src/App.js
@@ -51,11 +51,11 @@ export default connect(mapStateToProps)(App);
 const GlobalStyles = createGlobalStyle`
  html {
   font-size: 62.5%;
-}
-body {
-  @import url("https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap");
-  font-family: "Open Sans", sans-serif;
-  font-size: 1.6rem;
+  body {
+    @import url("https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap");
+    font-family: "Open Sans", sans-serif;
+    font-size: 1.6rem;
+  }
 }
 .wrapper {
   width: 80%;
