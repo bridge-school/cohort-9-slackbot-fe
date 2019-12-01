585ce3ca9ffab3e862f048c25f5882a559b6e03e
Zena Jun <zena.jun@gmail.com>
2019-11-30 23:37:20 -0500

    added dev tools for redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import messageReducer from "./messageReducer";
 import channelReducer from "./channelReducer";
@@ -9,12 +10,18 @@ const rootReducer = combineReducers({
   channels: channelReducer
   // 🔔 TO ADD --> archive: archiveReducer
 });
+
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(reduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(reduxThunk))
 );
+// const store = createStore(
+//   rootReducer,
+//   {},
+//   compose(
+//     applyMiddleware(reduxThunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
