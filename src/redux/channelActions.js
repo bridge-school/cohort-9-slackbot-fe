2cf5c5fd250bc74fef8c71fcd21b5e85cc9b5c6c
Zena Jun <zena.jun@gmail.com>
2019-12-02 22:36:04 -0500

    updated fetch thunk

--- src/redux/channelActions.js
+++ src/redux/channelActions.js
@@ -16,7 +16,7 @@ export const setChannelList = (channels = []) => ({
 
 // fetch channels thunk 💡
 export const fetchChannelsThunk = () => dispatch => {
-  setIsChannelsLoading(true);
+  dispatch(setIsChannelsLoading(true));
   return fetch(API_BASE_URL + "/channels")
     .then(res => res.json())
     .then(data => {
